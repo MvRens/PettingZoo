5fc5c1be12f6de2497a9b47ea0162c8a85a320c2
Mark van Renswoude <mark@x2software.net>
2022-01-03 17:44:20 +0100

    Implemented syntax highlighting for the message body in subscriber tabs

--- PettingZoo/UI/Tab/Subscriber/SubscriberView.xaml.cs
+++ PettingZoo/UI/Tab/Subscriber/SubscriberView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using ICSharpCode.AvalonEdit.Highlighting;
 
 namespace PettingZoo.UI.Tab.Subscriber
 {
@@ -13,6 +14,22 @@ namespace PettingZoo.UI.Tab.Subscriber
         {
             DataContext = viewModel;
             InitializeComponent();
+
+            // TODO wrap all this nonsense (which is a duplicate from PayloadEditorControl) in a UserControl
+            // should contain the border, one- or two-way Document binding and the automatic syntax highlighting based on a bound content-type
+            EditorBorder.BorderBrush = ReferenceControlForBorder.BorderBrush;
+            Editor.Options.IndentationSize = 2;
+
+            viewModel.PropertyChanged += (_, args) =>
+            {
+                if (args.PropertyName != nameof(viewModel.SelectedMessageBody))
+                    return;
+
+                Editor.Document.Text = viewModel.SelectedMessageBody;
+                Editor.SyntaxHighlighting = (viewModel.SelectedMessage?.Properties.ContentType ?? "") == "application/json"
+                        ? HighlightingManager.Instance.GetDefinition(@"Json")
+                        : null;
+            };
 
 
             if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
